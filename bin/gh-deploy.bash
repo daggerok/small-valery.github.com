4b4c83b4638ed1fd7c42571a3d6ad770cfb2f909
Maksim Kostromin <daggerok@gmail.com>
2016-10-17 07:21:44 +0300

    2016Y-10-17T07:10 dist subtree commit

--- bin/gh-deploy.bash
+++ bin/gh-deploy.bash
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 # build project
-npm i && npm run build -- --prod
+npm i && npm run build
 
 # comment out '/dist' line in .gitignore file:
 sed -i -e 's/^\/dist$/#\/dist/g' .gitignore
 
 # dist subtree commit:
-git add .
+git add dist
 git commit -am $(date +%YY-%m-%d'T'%H:%m)\ dist\ subtree\ commit
 
 # deploy build dir for github pages
-git subtree push --prefix dist master
-#git push origin $(git subtree split --prefix=dist --onto=origin/master):master
+git push origin $(git subtree split --prefix=dist --onto=origin/master):master
 
 # uncomment back to ignoring dist folder
 sed -i -e 's/^#\/dist$/\/dist/g' .gitignore
